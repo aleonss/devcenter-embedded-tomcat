d026c7cc86fc9e477d3541e2154b07abbd4addc0
aleonss <alvaro.leon@ug.uchile.cl>
2017-11-06 01:03:49 -0300

    zs

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -9,41 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-        name = "MyServlet", 
-        urlPatterns = {"/hello"}
-    )
+@WebServlet(name = "MyServlet", urlPatterns = { "/hello" })
 public class HelloServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        
-        
-        String res ="<html>\n" + 
-        		"<head>\n" + 
-        		"<title>dude</title>\n" + 
-        		"</head>\n" + 
-        		"<body>\n" + 
-        		"	<h2>hell yea</h2>"
-        		+ "<center>\n";
-        		
-        for(int i=0; i<800; i++) {
-            for(int j=0; j<i%(100+j); j++) {
-            	if(Math.random()>0.5)
-            		res+=" x ";
-            	else
-            		res+=" - ";
-            }
-            res+="<br>\n";
-        }
-        res+="</center>"
-        		+ "</body>\n" + 
-        		"</html>";
-        out.write(res.getBytes());
-        out.flush();
-        out.close();
-    }
-    
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ServletOutputStream out = resp.getOutputStream();
+
+		String res = "<html>\n" + "<head>\n" + "<title>dude</title>\n" + "</head>\n" + "<body>\n"
+				+ "	<h2>hell yea</h2>" + "<center>\n";
+
+		for (int i = 0; i < 800; i++) {
+			for (int j = 0; j < i % (100 + j); j++) {
+				if (Math.random() > 0.5)
+					res += " x ";
+				else
+					res += " - ";
+			}
+			res += "<br>\n";
+		}
+		res += "</center>" + "</body>\n" + "</html>";
+		out.write(res.getBytes());
+		out.flush();
+		out.close();
+	}
+
 }
